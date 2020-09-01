334b3962eee3c04015d831dd1af4dced9fad0b20
Ryoga Kitagawa <ryoga.kitagawa@gmail.com>
2020-09-01 20:30:04 +0900

    init

---
+++ index.js
@@ -0,0 +1,50 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'prettier/@typescript-eslint',
+        'prettier/react',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint'],
+    settings: {
+        'import/resolver': {
+            node: {
+                extensions: ['.ts', '.tsx'],
+            },
+        },
+    },
+    rules: {
+        'no-use-before-define': ['error', { functions: false }],
+        'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'import/extensions': [
+            'error',
+            'ignorePackages',
+            { js: 'never', jsx: 'never', mjs: 'never', ts: 'never', tsx: 'never' },
+        ],
+        'import/order': [2, { alphabetize: { order: 'asc' } }],
+        'no-unused-vars': [
+            'error',
+            { varsIgnorePattern: '^_', argsIgnorePattern: '^_' },
+        ],
+    },
+};
